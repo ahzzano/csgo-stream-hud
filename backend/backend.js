266f61c7fb7b0be4fe6c1d9caa641f08bf90fb3a
W. Santos <w.santos.gg@gmail.com>
2022-05-23 19:00:01 +0800

    created a basic backend using expressjs

---
+++ backend/backend.js
@@ -0,0 +1,21 @@
+import {express} from 'express'
+import {cors} from 'cors'
+
+const app = express()
+
+let state = {}
+
+app.use(cors())
+
+app.post('/' , (req, res) => {
+    console.log(res.body)
+    res.send('hello')        
+})
+
+app.get('/', (req, res) => {
+
+})
+
+export {
+    app
+}
