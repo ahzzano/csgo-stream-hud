edc14effface18caa4e7e5ad619f6dbbf4a02a92
W. Santos <w.santos.gg@gmail.com>
2022-05-23 19:18:11 +0800

    post request now receives proper data

--- backend/backend.js
+++ backend/backend.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import bodyParser from 'body-parser'
 
 const app = express()
 
 let state = {}
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.post('/' , (req, res) => {
     console.log(req.body)
