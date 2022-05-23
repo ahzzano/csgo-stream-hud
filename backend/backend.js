7989214c04b72cbf414483398b02e725f7cd5ff9
W. Santos <w.santos.gg@gmail.com>
2022-05-23 19:06:27 +0800

    fixed some errors with the backend

--- backend/backend.js
+++ backend/backend.js
@@ -1,5 +1,5 @@
-import {express} from 'express'
-import {cors} from 'cors'
+import express from 'express'
+import cors from 'cors'
 
 const app = express()
 
@@ -17,5 +17,5 @@ app.get('/', (req, res) => {
 })
 
 export {
-    app
+    app as app
 }
---
+++ run_backend.js
@@ -0,0 +1,5 @@
+import {app} from './backend/backend.js'
+
+app.listen(4000, () => {
+    console.log('starting server at port 4000')
+})
