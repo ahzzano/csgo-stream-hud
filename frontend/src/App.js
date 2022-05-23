2c10e9d7f0361a2723df433b9db42c9f4a12bc45
W. Santos <w.santos.gg@gmail.com>
2022-05-24 07:58:25 +0800

    changed localhost to 127.0.0.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch('http://localhost:4000').then((response) => {
+      fetch('http://127.0.0.1:4000').then((response) => {
         response.json().then((body) => {
           setGameState(body)
         })
