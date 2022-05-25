a5145709d8ff2ea0a96abac1c7a66bb9bd5a6527
W. Santos <w.santos.gg@gmail.com>
2022-05-25 11:48:27 +0800

    added an armor value to the players

--- frontend/src/Player/Player.js
+++ frontend/src/Player/Player.js
@@ -1,10 +1,12 @@
 import './Player.css'
-import Health from './health-icon.png'
+import HealthIcon from './health-icon.png'
+import ArmorIcon from './armor-icon.png'
 
 function Player(props) {
   let player = props.playerObject
 
   let health = player.player_status.health
+  let armor = player.player_status.armor
 
   let direction = props.align == 'left' ? 'to-left' : 'to-right'
 
@@ -31,7 +33,7 @@ function Player(props) {
   }    
   
   return (
-    <div style={{width: 400, color: "white", height: 60}} className="player">
+    <div style={{width: 450, color: "white", height: 60}} className="player">
         <div className={`flex ${direction}`}>
 
           <img src={"https://nadeko.bot/static/media/nadeko-top.ad6cc06a.png"} width={60} height={60}/>
@@ -41,7 +43,11 @@ function Player(props) {
             <div className={`flex ${direction} inside-player`}>
               <span className={'game-variables'}>
                 {health}
-                <img src={Health} width={20} height={20}/>
+                <img src={HealthIcon} width={20} height={20}/>
+              </span>
+              <span className={'game-variables'}>
+                {armor}
+                <img src={ArmorIcon} width={20} height={20}/>
               </span>
               <span>{player.name}</span>
             </div>
